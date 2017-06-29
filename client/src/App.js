dd6ced0b04ab1e704d57751bf9420ae919cc7210
李旭 <lixu@lixudeMacBook-Pro.local>
2017-06-29 22:14:04 +0800

    ui更新

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Header from './Components/Header'
+import SideBar from './Components/SideBar'
+import PlayBar from './Components/PlayBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="app">
+          <Header></Header>
+          <div className="container">
+            <SideBar></SideBar>
+            <main></main>
+          </div>
+          <PlayBar></PlayBar>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ client/src/Components/PersonLetter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PersonLetter extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+PersonLetter.propTypes = {
+
+};
+
+export default PersonLetter;
